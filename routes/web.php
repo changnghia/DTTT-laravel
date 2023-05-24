8474f33e42e4fc7caace219351c656934f27af32
ADMIN\Admin <changnghia2307>
2023-05-24 07:41:17 +0700

    fist

---
+++ routes/web.php
@@ -0,0 +1,132 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+use  App\Models\Cart;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/index.html',[CategoryController::class,'index'] )->name('indexuser');
+Route::get('/fruit.html',[CategoryController::class,'indexfruit'] )->name('product.all');
+Route::post('/search',[CategoryController::class,'search'] )->name('product.search');
+Route::get('/fruit',[CategoryController::class,'indexfruitDK'] )->name('product.dm');
+Route::get('/chitietsanpham',[CategoryController::class,'chitietsp'] )->name('chitietsp');
+Route::get('/gio-hang-cua-ban',[CartController::class,'tanggiam'])->name('tanggiam');
+Route::post('/chitietsanpham/themvaogio',[CategoryController::class,'themvaogio'])->name('themvaogio')->middleware(['auth']);
+
+
+Route::get('/don-hang-cua-ban',[CategoryController::class,'donhanguser'])->name('donhanguser')->middleware(['auth']);
+Route::get('/chi-tiet-don-hang-cua-ban',[CategoryController::class,'chitietdonhanguser'])->name('chitietdonhanguser')->middleware(['auth']);
+Route::get('/huy-don-hang-cua-ban',[CategoryController::class,'huydonhang'])->name('huydonhang')->middleware(['auth']);
+
+
+Route::get('/Dathang', function () {
+    if(auth()->check()){
+        $hover=Cart::where('id_user',auth()->user()->id)->get();
+        return view('hoanthanh')->with('hover',$hover);
+    }else{
+        return view('hoanthanh')->with('kiemtralogin','check');
+    }
+})->name('hoanthanh');
+
+Route::get('/about.html', function () {
+    if(auth()->check()){
+    $hover=Cart::where('id_user',auth()->user()->id)->get();
+    return view('about')->with('hover',$hover);
+}else{
+    return view('about')->with('kiemtralogin','check');
+}
+});
+
+Route::get('/thanhtoan.html', function () {
+    $cart= Cart::where('id_user',auth()->user()->id)->get();
+    $hover=Cart::where('id_user',auth()->user()->id)->get();
+    return view('thanhtoan')->with('hover',$hover)->with('cart',$cart);
+})->name('thanhtoan')->middleware(['auth']);
+Route::get('/contact.html', function () {
+    if(auth()->check()){
+        $hover=Cart::where('id_user',auth()->user()->id)->get();
+        return view('contact')->with('hover',$hover);
+    }else{
+        return view('contact')->with('kiemtralogin','check');
+    }
+});
+
+Route::get('/testimonial.html', function () {
+
+    if(auth()->check()){
+        $hover=Cart::where('id_user',auth()->user()->id)->get();
+        return view('testimonial')->with('hover',$hover);
+    }else{
+        return view('testimonial')->with('kiemtralogin','check');
+    }
+});
+Route::resource('/',CategoryController::class);
+Route::resource('/Cart',CartController::class)->middleware(['auth']);
+Route::resource('/Product',ProductController::class);
+
+Route::prefix('admin')->middleware('Role')->group(function (){    
+Route::get('/bangdieukhien', function () {
+ 
+    return view('spica.index');
+
+})->name('bangdieukhien');
+
+Route::get('/danhsachsanpham', [ProductController::class,'index'] )->name('danhsachsanpham');
+Route::post('/search-tukhoa', [ProductController::class,'searchdssp'] )->name('searchdssp');
+Route::post('/searchdsspdm', [ProductController::class,'searchdsspdm'])->name('searchdsspdm');
+
+
+
+Route::get('/DanhMucAdmin', [CategoryController::class,'DanhMucAdmin'] )->name('DanhMucAdmin');
+Route::DELETE('/deletedm/{id}', [CategoryController::class,'deletedm'] )->name('deletedm');
+Route::get('/{id}/edit', [CategoryController::class,'edit'] )->name('editDanhMucAdmin');
+Route::PUT('/update/{id}', [CategoryController::class,'update'] )->name('updateDanhMucAdmin');
+Route::get('/themdanhmuc', [CategoryController::class,'create'] )->name('createDanhMucAdmin');
+Route::post('/Taodanhmuc', [CategoryController::class,'store'] )->name('storeDanhMucAdmin');
+
+Route::get('/listdonhang', [CartController::class,'indexbill'] )->name('indexbill');
+
+Route::get('/{Cart}/editdonhang', [CartController::class,'editdonhang'])->name('editdonhang');
+Route::get('/viewctbill', [CartController::class,'indexctbill'])->name('indexctbill');
+
+Route::post('/{Cart}/updatetrangthai', [CartController::class,'updatetrangthai'])->name('updatetrangthai');
+
+Route::PUT('/{Cart}/updatedonhang', [CartController::class,'updatedonhang'])->name('updatedonhang');
+
+Route::get('/danhsach-taikhoan', [CartController::class,'taikhoan'])->name('taikhoan');
+Route::get('/{Cart}/taikhoan-edit', [CartController::class,'edittaikhoan'])->name('edittaikhoan');
+Route::PUT('/{Cart}/updatetaikhoan', [CartController::class,'updatetaikhoan'])->name('updatetaikhoan');
+Route::DELETE('/xoataikhoan/{Cart}', [CartController::class,'xoataikhoan'] )->name('xoataikhoan');
+Route::get('/phanquyen', [CartController::class,'phanquyen'])->name('phanquyen');
+Route::post('/{Cart}/updaterole', [CartController::class,'updaterole'])->name('updaterole');
+Route::post('/{Cart}/updateroleuser', [CartController::class,'updateroleuser'])->name('updateroleuser');
+
+Route::get('/Thongke', [CartController::class,'Thongke'])->name('Thongke');
+Route::GET('/searchmonth', [CartController::class,'searchmonth'])->name('searchmonth');
+
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
